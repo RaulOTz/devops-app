ab6e1da3f5299dd963dcc9b4718f35b076f287be
Raul Zatarain <raulzatarain@MacBook-Pro-de-Raul-6.local>
2026-04-17 19:56:52 -0700

    app.js update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps funcionando 🚀');
+  res.send('Deployed 🚀');
 });
 
 app.listen(3000, () => {
